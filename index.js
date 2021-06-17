8b0f9526264a789604473818ed6c27205cbf4ae6
gabe-morris <gabe.e.morris@gmail.com>
2021-06-17 12:53:39 -0500

    finished task 2

--- index.js
+++ index.js
@@ -75,7 +75,24 @@ class Airplane {
   */
   
  class Car {
-    
+   constructor(model,milesPerGallon){
+     this.model = model;
+     this.milesPerGallon = milesPerGallon;
+     this.tank = 0;
+     this.odometer = 0;
+   }
+    fill(gallons){
+      this.tank += gallons;
+    }
+    drive(distance){
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+      if(this.tank < distance/this.milesPerGallon){
+        this.odometer += this.tank * this.milesPerGallon;
+        this.tank = 0;
+        return `I ran out of fuel at ${this.odometer} miles!`;
+      }
+    }
   }
   
   /*
